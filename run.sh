59fdfdce252c7abec1661729471ad90fed6c3676
gdymind <gdymind@gmail.com>
2023-09-23 20:34:50 -0500

    bind memory and CPU in the same NUMA node

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # dataset
-datasets="books fb osm"
+datasets="books"
 
 # Read/Insert ratios
 ratios="1:0"
@@ -14,38 +14,39 @@ sbuck_sizes="8"
 dbuck_sizes="16384"
 
 # cores
-# cores="0 1 2 3 4 5 6 7 8 9 10 11"
-cores="0 1"
+cores="0"
+# cores="0 1"
 
 #indexes
 indexes="alex lipp finedex pgm"
 
 # # Iterate the string array using for loop
-# for ratio in $ratios; do
-#     for dataset in $datasets; do
-#         # bli
-#         for sbuck_size in $sbuck_sizes; do
-#             for dbuck_size in $dbuck_sizes; do
-#                 for c in $cores; do
-#                     command="taskset -c $c ./microbench --keys_file=../datasets/${dataset} --keys_file_type=binary --read=${ratio%:*} --insert=${ratio#*:} --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=${sbuck_size} --bli_dbuck_size=${dbuck_size} --bli_initial_filled_ratio=0.6"
-#                     echo "command is $command"
-#                     taskset -c $c $command
-#                     sleep 1
-#                 done
-#             done
-#         done
+for ratio in $ratios; do
+    for dataset in $datasets; do
+        # bli
+        for sbuck_size in $sbuck_sizes; do
+            for dbuck_size in $dbuck_sizes; do
+                for c in $cores; do
+                    # command="taskset -c $c ./microbench --keys_file=../datasets/${dataset} --keys_file_type=binary --read=${ratio%:*} --insert=${ratio#*:} --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=${sbuck_size} --bli_dbuck_size=${dbuck_size} --bli_initial_filled_ratio=0.6  --error_bound=4.8"
+                    command="numactl --cpunodebind=0 --membind=0  ./microbench --keys_file=../datasets/${dataset} --keys_file_type=binary --read=${ratio%:*} --insert=${ratio#*:} --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=${sbuck_size} --bli_dbuck_size=${dbuck_size} --bli_initial_filled_ratio=0.6  --error_bound=4.8"
+                    echo "command is $command"
+                    taskset -c $c $command
+                    sleep 1
+                done
+            done
+        done
 
-#         # others
-#         for index in $indexes; do
-#             for c in $cores; do
-#                 command="taskset -c $c ./microbench --keys_file=../datasets/${dataset} --keys_file_type=binary --read=${ratio%:*} --insert=${ratio#*:} --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=${index} --memory"
-#                 echo "command is $command"
-#                 taskset -c $c $command
-#                 sleep 1
-#             done
-#         done
-#     done
-# done
+        # # others
+        # for index in $indexes; do
+        #     for c in $cores; do
+        #         command="taskset -c $c ./microbench --keys_file=../datasets/${dataset} --keys_file_type=binary --read=${ratio%:*} --insert=${ratio#*:} --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=${index} --memory"
+        #         echo "command is $command"
+        #         taskset -c $c $command
+        #         sleep 1
+        #     done
+        # done
+    done
+done
 
 
 
@@ -89,4 +90,5 @@ indexes="alex lipp finedex pgm"
 # done
 
 
-taskset -c 0 ./microbench --keys_file=../datasets/books --keys_file_type=binary --read=1 --insert0 --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=8 --bli_dbuck_size=16384 --bli_initial_filled_ratio=0.6 --error_bound=4.8
+# taskset -c 0 ./microbench --keys_file=../datasets/books --keys_file_type=binary --read=1 --insert0 --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=8 --bli_dbuck_size=16384 --bli_initial_filled_ratio=0.6 --error_bound=4.8
+# taskset -c 1 ./microbench --keys_file=../datasets/books --keys_file_type=binary --read=1 --insert0 --operations_num=100000000 --table_size=200000000 --init_table_ratio=0.5 --thread_num=1 --index=buckindex --memory --bli_sbuck_size=8 --bli_dbuck_size=16384 --bli_initial_filled_ratio=0.6 --error_bound=4.8
